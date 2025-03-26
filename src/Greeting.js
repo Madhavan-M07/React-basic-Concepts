c95ce54e2b65fe2d25de8f1a4570009ca619064b
madhavan-m07 <163586140+Madhavan-M07@users.noreply.github.com>
2025-03-26 05:57:53 +0000

    Changes made

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,7 +1,12 @@
 import React from "react";
-function Greeting() {
+function Greeting(props) {
   const name = "Macha";
-  return <h1>Hello , {name}!</h1>;
+  return( 
+  <>
+  <h1>Hello , {name}!</h1>
+  <h2>Hello , Im {props.name}!</h2>
+  </ >
+);
 }
 
 export default Greeting;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Greeting from "./Greeting.js";
+import Button from "./Button.js";
 
 import App from "./App";
 
@@ -10,7 +11,10 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-    <Greeting />
+    <Button />
+    <Greeting name ="Madhavan"/>
+    <Greeting name ="Maddy"/>
+    <Button />
   </StrictMode>
 );
 
@@ -24,3 +28,16 @@ root.render(
 // Component Function: Greeting() returns a JSX element.
 
 // Exporting: The component is exported to be used in other files.
+
+// Here Button is the Component ..Where I have resued the component twice
+// Components make the UI reusable.
+// They Keep Code Clean and Structured .
+// You can use the same Component multiple times .
+
+//Jsx must be wrap in to the single parent called fragment or div .
+// Passing Data Between Components Using Props in React
+// Props ( Short form of properties) allow you to pass data from one component (parent)
+// to child component
+// Props helps to pass dynamic data to child components.
+// Props are read  only (Cannot be modified inside the Child Component)
+// multiple 
